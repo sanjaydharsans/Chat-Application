a81aede24e88f0992abbe4b93f036ad3af720736
sanjaydharsans <sanjaydharsans@gmail.com>
2023-04-28 22:29:36 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,64 @@
+const path = require("path");
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
+const Filter = require("bad-words");
+const { generateMessage, generateLocationMessage } = require("./utils/messages");
+const { addUser, removeUser, getUser, getUsersInRoom } = require("./utils/users");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+const publicDirectoryPath = path.join(__dirname, "../public");
+
+app.use(express.static(publicDirectoryPath));
+
+io.on("connection", socket => {
+  console.log("New user joined!!!");
+
+  socket.on("join", (options, callback) => {
+    const { error, user } = addUser({ id: socket.id, ...options });
+    if (error) {
+      return callback(error);
+    } else {
+      socket.join(user.room);
+
+      socket.emit("message", generateMessage("Admin", "Welcome!"));
+      socket.broadcast.to(user.room).emit("message", generateMessage("Admin", `${user.username} joined!`));
+      io.to(user.room).emit("roomData", {
+        room: user.room,
+        users: getUsersInRoom(user.room)
+      });
+
+      callback();
+    }
+  });
+
+  socket.on("sendMessage", (message, callback) => {
+    const user = getUser(socket.id);
+    const filter = new Filter();
+
+    if (filter.isProfane(message)) {
+      return callback("Profanity is not allowed!");
+    } else {
+      io.to(user.room).emit("message", generateMessage(user.username, message));
+      callback();
+    }
+  });
+
+  socket.on("disconnect", () => {
+    const user = removeUser(socket.id);
+    if (user) {
+      io.to(user.room).emit("message", generateMessage("Admin", `${user.username} left!`));
+      io.to(user.room).emit("roomData", {
+        room: user.room,
+        users: getUsersInRoom(user.room)
+      });
+    }
+  });
+});
+
+server.listen(3000, () => {
+  console.log(`Server is running on port🚀: 3000!`);
+});
